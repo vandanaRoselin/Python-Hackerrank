5340e4d21b389ea8b8ac3e96adfe92ffdfe779f3
vandanaRoselin <140235363+vandanaRoselin@users.noreply.github.com>
2024-06-11 12:52:42 +0530

    Create Slicing.py

---
+++ Slicing.py
@@ -0,0 +1,5 @@
+y_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(my_list[-3:])      # Output: [8, 9, 10] (last three elements)
+print(my_list[:-2])      # Output: [1, 2, 3, 4, 5, 6, 7, 8] (all elements except last two)
+print(my_list[-5:-2])    # Output: [6, 7, 8] (elements from 6th to 8th, inclusive)
+print(my_list[-1::-1])   # Output: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1] (reverse the list)
